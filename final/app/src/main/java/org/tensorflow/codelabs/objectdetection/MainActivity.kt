087b04cfc98014c8928e4c94c822f7ccb6dc6fab
huyan27 <annh1@vmodev.com>
2021-10-12 10:28:42 +0700

    check

--- final/app/src/main/java/org/tensorflow/codelabs/objectdetection/MainActivity.kt
+++ final/app/src/main/java/org/tensorflow/codelabs/objectdetection/MainActivity.kt
@@ -1,18 +1,3 @@
-/**
- * Copyright 2021 Google LLC
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * https://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 
 package org.tensorflow.codelabs.objectdetection
 
@@ -126,7 +111,7 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
                 .build()
         val detector = ObjectDetector.createFromFileAndOptions(
                 this,
-                "modelLite2.tflite",
+                "modelLite0.tflite",
                 options
         )
 
@@ -158,6 +143,7 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         for ((i, obj) in results.withIndex()) {
             val box = obj.boundingBox
 
+            Log.d(TAG,"====================================")
             Log.d(TAG, "Detected object: ${i} ")
             Log.d(TAG, "  boundingBox: (${box.left}, ${box.top}) - (${box.right},${box.bottom})")
 
